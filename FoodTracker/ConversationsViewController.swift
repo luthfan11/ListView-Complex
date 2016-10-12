57b6a2ffcc64bd25154c1964252965cebec02092
luthfan <luthfan.a.haq@gmail.com>
2016-10-12 09:22:30 +0700

    delete MealViewController.swift

---
+++ FoodTracker/ConversationsViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MealViewController.swift
+//  FoodTracker
+//
+//  Created by Jane Appleseed on 5/23/15.
+//  Copyright © 2015 Apple Inc. All rights reserved.
+//  See LICENSE.txt for this sample’s licensing information.
+//
+
+import UIKit
+
+class ConversationsViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    // MARK: Properties
+    
+//    @IBOutlet weak var nameTextField: UITextField!
+//    @IBOutlet weak var photoImageView: UIImageView!
+//    @IBOutlet weak var ratingControl: RatingControl!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Handle the text field’s user input through delegate callbacks.
+//        nameTextField.delegate = self
+    }
+
+}
+
